17cdc34efa4bf93aee0a2306ec45f7f9603667b7
K Gautam Pai <gautampai@gautampai.com>
2025-08-12 14:49:05 +0530

    refactor(nvim): mv leader key setup into separate keymaps file

--- nvim/init.lua
+++ nvim/init.lua
@@ -86,12 +86,6 @@ P.S. You can delete this when you're done too. It's your config now! :)
 
 require("gp.remap")
 
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
 
